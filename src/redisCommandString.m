824a9f0904f1031c06f654c5ad18e79f30259dd9
Dan Swain <dswain@princeton.edu>
2012-11-11 22:22:40 -0500

    Modify the way the string is written - works with Redis 2.2.12

--- src/redisCommandString.m
+++ src/redisCommandString.m
@@ -9,7 +9,7 @@ cmd = sprintf('*%d', n_words);
 for ix = 1 : n_words
   word = words{ix};
   word_length = numel(word);
-  cmd = sprintf('%s\r\n$%d\r\n%s', cmd, word_length, word);
+  cmd = sprintf('%s\n$%d\n%s', cmd, word_length, word);
 end
 
 % note the final \r\n is added by the port

e4d22c66160ee694eef4b748b9093d1f4d961268
Dan Swain <dswain@princeton.edu>
2012-11-12 23:10:38 -0500

    Add redisHGetAlll

---
+++ src/redisHGetAll.m
@@ -0,0 +1,24 @@
+function [Struct, R, S] = redisHGetAll(R, key)
+
+S = 'OK';
+Fields = {};
+Values = {};
+
+if ~strcmp(R.status, 'open')
+  S = 'ERROR - NO CONNECTION';
+  return;
+end
+
+[Response, R, S] = redisCommand(R, redisCommandString(sprintf('HGETALL %s', key)));
+
+if length(Response) == 0,
+  S = 'ERROR - NO RESPONSE FROM SERVER';
+  return
+end
+
+Parts = redisParseBulkReply(Response);
+
+Fields = Parts(1 : 2 : end);
+Values = Parts(2 : 2 : end);
+
+Struct = cell2struct(Values, Fields, 1);
